fa88671295b2e71ab8d040fb7d05581b337a2a93
Liza Molina <lnmolina.23@gmail.com>
2023-05-08 15:06:33 -0400

    java

---
+++ index.js
@@ -0,0 +1,8 @@
+
+    const modal = document.querySelector('.modal')
+    const button1 = document.querySelector('#button1')
+
+    button1.addEventListener('click', () =>{
+        modal.classList.remove('hidden')
+        modal.classList.add('visible')
+})
