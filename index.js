a95cad1c9e0dc2b29aa99182d1f499adeabb8bf4
Liza Molina <lnmolina.23@gmail.com>
2023-05-08 17:34:33 -0400

    tercera modificacion

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 
     const modal = document.querySelector('.modal')
     const button1 = document.querySelector('#button1')
+    const closeButton = document.querySelector('.modal__content--close')
 
     button1.addEventListener('click', () =>{
         modal.classList.remove('hidden')
         modal.classList.add('visible')
-})
+    })
+
+    closeButton.addEventListener('click', () =>{
+        crossOriginIsolated.log('here') 
+        modal.classList.add('hidden')
+        modal.classList.remove('visible')
+    })
+
+
